b4a46f865c2b86aa6a837d5bc069ae7f207ab3ac
Jeff Spagnola <65861474+spags093@users.noreply.github.com>
2021-02-14 23:13:48 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# main chatbot script
+
+# Imports
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+# stemmer = LancasterStemmer()
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+import json
+
